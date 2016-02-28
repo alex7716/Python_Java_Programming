78e8b283eb7d5b597e3d3313227b59d30dd2a736
DimaS64 <dstankevich04@gmail.com>
2016-02-28 22:11:30 +0200

    Added files via upload

---
+++ If-else-elseif.py
@@ -0,0 +1,36 @@
+Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:32:19) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> age = 25
+>>> if age > 20:
+	print('You are too old!')
+else:
+	print('You are too young!')
+
+You are too old!
+>>> age = 25
+>>> if age > 20:
+	    print('You are too old!')
+
+	    
+You are too old!
+>>> age = 12
+>>> if age == 12:
+	print("aaaaa")
+else:
+	print("Shhhhhhhhhh")
+
+	
+aaaaa
+>>> age = 12
+>>> if age == 10:
+	print("a-a-a")
+elif age == 11:
+	print("be-be-be")
+elif age == 12:
+	print("age12!")
+else:
+	print("o-o-o!")
+
+	
+age12!
+>>> 
